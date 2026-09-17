827b3d0a9a811686f8fa9610e93f4fb23ebcca60
Alex Fomin <magritte@tutamail.com>
2026-09-17 16:07:22 +0200

    Attar 0.1.0-dev.7409.ge99759d41293

--- Formula/attar.rb
+++ Formula/attar.rb
@@ -1,9 +1,9 @@
 class Attar < Formula
   desc "Attar developer toolkit with CLI, compiler, and SDK"
   homepage "https://github.com/occam-tech/attar"
-  url "https://github.com/occam-tech/attar-releases/releases/download/v0.1.0-dev.7407.gecff84b5160a/attar-0.1.0-dev.7407.gecff84b5160a-aarch64-apple-darwin.tar.gz"
-  sha256 "cd8c69242397b5b4f4be6e280e39e33d0dbbe36c3474b4c32c29fd7fed73a9da"
-  version "0.1.0-dev.7407.gecff84b5160a"
+  url "https://github.com/occam-tech/attar-releases/releases/download/v0.1.0-dev.7409.ge99759d41293/attar-0.1.0-dev.7409.ge99759d41293-aarch64-apple-darwin.tar.gz"
+  sha256 "8a6549451965a439dd023a38bc2f6914e7ba1748782c5186ef098f12718c3397"
+  version "0.1.0-dev.7409.ge99759d41293"
 
   depends_on arch: :arm64
   depends_on macos: :sequoia
